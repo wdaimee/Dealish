ccdff57199b955fcd2abd85aca4264b001aa7121
Waqas D <wdaimee@gmail.com>
2020-04-11 15:51:12 -0400

    added urls.py file to main_app and add boilerplate. Added Restaurant model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Restraurant(models.Model):
+    name: models.CharField(max_length=200)
+    address: models.CharField(max_length=250)
+    Phone_Number: models.CharField(max_length=20)
