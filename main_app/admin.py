6d4d0cf94a0a2515094b31eec2e3efc94948585f
Waqas D <wdaimee@gmail.com>
2020-04-11 20:27:59 -0400

    added all models to admin.py file

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Deals)
+admin.site.register(Review)
+admin.site.register(Favourite)
+admin.site.register(Notes)
