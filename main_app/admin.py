cc6cb829e5ff2b328142ccb0a90ca5e8b3738094
Waqas D <wdaimee@gmail.com>
2020-04-13 17:32:21 -0400

    corrected plural Notes and Reviews to signular

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Restaurant)
-admin.site.register(Deals)
+admin.site.register(Deal)
 admin.site.register(Review)
 admin.site.register(Favourite)
-admin.site.register(Notes)
+admin.site.register(Note)
