be34dde94724f52571a5cb76002e9a8bfe96eac7
Waqas D <wdaimee@gmail.com>
2020-04-13 17:33:59 -0400

    added fields to Restaurant model for coordinates and corrected plural models to signular (ex. Notes to Note)

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,39 @@
+from django.contrib.gis.db import models as gis_models
+from django.contrib.gis import geos
 from django.db import models
 from django.contrib.auth.models import User
+import geopy.geocoders
+from geopy.geocoders import GoogleV3
+from urllib.error import URLError
 
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=250)
+    city = models.CharField(max_length=100, default='Toronto')
     phone_number = models.CharField(max_length=20)
     picture = models.CharField(max_length=20) # possibly change to imagefield?
     Likes = models.IntegerField(default=0)
+    coordinates = gis_models.PointField(u"longitdue/latitude", geography=True, blank=True, null=True)
 
-class Deals(models.Model):
+    def __str__(self):
+        return self.name
+
+    def save(self, **kwargs):
+        if not self.coordinates:
+            address = f"{self.address} {self.city}"
+            address = address.encode('utf-8')
+            geocoder = GoogleV3(api_key='AIzaSyAp_xdxr3d7wTp-g2xrrP5vTFAfipgSmfI')
+            try:
+                _, latlon = geocoder.geocode(address)
+            except (URLError, ValueError, TypeError):
+                pass
+            else:
+                point = f"POINT({latlon[1]} {latlon[0]})"
+                self.coordinates = geos.fromstr(point)
+        super(Restaurant, self).save()
+
+class Deal(models.Model):
     name = models.CharField(max_length=200)
     price = models.CharField(max_length=10)
     description = models.TextField(max_length=250)
@@ -24,6 +48,6 @@ class Favourite(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Notes(models.Model):
+class Note(models.Model):
     text = models.TextField(max_length=250)
     favourite = models.ForeignKey(Favourite, on_delete=models.CASCADE)
