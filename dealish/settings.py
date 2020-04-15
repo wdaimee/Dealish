619e6b018a4cb89ddab891299e46d6a2e9078b9b
Waqas D <wdaimee@gmail.com>
2020-04-15 11:34:01 -0400

    added jsonify to installed apps in settings.py

--- dealish/settings.py
+++ dealish/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'geopy.geocoders',
     'django.contrib.gis',
+    'jsonify',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
