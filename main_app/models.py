fbc303229c23c929f46637e37d3d0ef843076b47
Waqas D <wdaimee@gmail.com>
2020-04-15 11:51:07 -0400

    updated restaurant model to add lat/lng fields, changed Likes filed to lower case likes, updated save method

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,7 @@ from geopy.geocoders import GoogleV3
 from urllib.error import URLError
 import os
 
+
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
@@ -14,14 +15,16 @@ class Restaurant(models.Model):
     city = models.CharField(max_length=100, default='Toronto')
     phone_number = models.CharField(max_length=20)
     picture = models.CharField(max_length=20) # possibly change to imagefield?
-    Likes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    lng = models.DecimalField(max_digits=8, decimal_places=3, default=0)
+    lat = models.DecimalField(max_digits=8, decimal_places=3, default=0)
     coordinates = gis_models.PointField(u"longitdue/latitude", geography=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
     def save(self, **kwargs):
-        if not self.coordinates:
+        if self.lng == 0.000 and self.lat == 0.00:
             address = f"{self.address} {self.city}"
             address = address.encode('utf-8')
             key = os.environ['GOOGLE_API_KEY']
@@ -32,6 +35,8 @@ class Restaurant(models.Model):
                 pass
             else:
                 point = f"POINT({latlon[1]} {latlon[0]})"
+                self.lat = latlon[0]
+                self.lat = latlon[1]
                 self.coordinates = geos.fromstr(point)
         super(Restaurant, self).save()
 
